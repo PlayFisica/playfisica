e537439234db2dcddf7dbf3aec4ce6ee8dbae565
GustavoFS <gustavoferreirasantosg@gmail.com>
2025-09-25 19:30:50 -0300

    c5

--- js/ang.js
+++ js/ang.js
@@ -1,72 +1,116 @@
-let   y_r = 400,y_t1_1 = 500,y_t1_2 = 550,
-y_t1_3 = 550,y_t2_1 = 500,y_t2_2 = 550,
-y_t2_3 = 550,y_t3_1 = 350,y_t3_2 = 400,y_t3_3 = 400;
-let y_c1=550,y_c2=550,y_c3=550;
-let velocidade=0, aceleracao=0.3;
-let velocidade2=0, aceleracao2=0.1;
+//Declara a Posicão e a velocidade do boneco
+let posx_b=880,posy_b=230,velx_b=5,vely_b=0;
+
+//Declara a posicão e a velocidade do carro
+let posx_c=750,velx_c=5;
+
+//Declara a gravidade
+let gravidade=0.03;
+
 function setup() {
-  createCanvas(800,700);
+  //Cria a tela
+  createCanvas(1000, 400);
 }
 
 function draw() {
-  background(0,200,255);
+  //Dar cor ao plano de fundo da tela
+  background(126, 213, 242);
   
+  //Sol
+  strokeWeight (1);
   fill(255,255,0);
-  noStroke();
-  circle(100,160,115);
+  circle (100,100,100);
   
-  stroke(0);
-  line(100,50,120,70);
-  line(120,70,135,50);
-  line(200,155,220,180);
-  line(220,180,240,155);
-  line(400,280,420,300);
-  line(420,300,440,280);
-  line(500,330,520,350);
-  line(520,350,540,330);
-  line(650,200,670,220);
-  line(670,220,690,200);
+  //Montanhas
+  fill (48, 217, 79);
+  ellipse (150,300,300,400);
+  ellipse (350,300,300,250);
+  fill (48, 217, 79);
+  ellipse (850,320,400,200);
+  fill (87, 242, 116);
+  ellipse (550,300,400,300);
   
-  noStroke();
-  fill (255,150,0,220);
-  quad (0,550,0,700,400,700,400,550);
-  fill (0,50,255);
-  quad (400,550,400,700,800,700,800,550);
+  //Rodovia
+  fill (50);
+  rect (0,300,1000,100);
   
-  fill(150,80,0);
-  quad (100,425,100,550,150,550,150,425);
+  //Sinalização da Rodovia
+  fill (255);
+  rect (20,340,50,10);
+  rect (100,340,50,10);
+  rect (180,340,50,10);
+  rect (260,340,50,10);
+  rect (340,340,50,10);
+  rect (420,340,50,10);
+  rect (500,340,50,10);
+  rect (580,340,50,10);
+  rect (660,340,50,10);
+  rect (740,340,50,10);
+  rect (820,340,50,10);
+  rect (900,340,50,10);
+  rect (980,340,50,10);
   
-  fill(0,150,0,200);
-  circle (70,420,70);
-  circle (125,420,70);
-  circle (175,420,70);
-  circle (100,375,70);
-  circle (150,375,70);
+  //Corpo do boneco
+  strokeWeight (4);
+  line (posx_b,posy_b,posx_b+50,posy_b);
+  line (posx_b+50,posy_b,posx_b+80,posy_b-10);
+  line (posx_b+50,posy_b,posx_b+80,posy_b+10);
+  line (posx_b+20,posy_b,posx_b+40,posy_b-20);
+  line (posx_b+20,posy_b,posx_b+40,posy_b+20);
   
-  fill (255,0,0);
-  rect (270,y_r,50,150);
-  fill (0,0,255);
-  triangle (270,y_t1_1,220,y_t1_2,270,y_t1_3);
-  triangle (320,y_t2_1,320,y_t2_2,370,y_t2_3);
-  triangle (295,y_t3_1,255,y_t3_2,335,y_t3_3);
-  velocidade = velocidade + aceleracao;
-  y_r = y_r - velocidade;
-  y_t1_1 = y_t1_1 - velocidade;
-  y_t1_2 = y_t1_2 - velocidade;
-  y_t1_3 = y_t1_3 - velocidade;
-  y_t2_1 = y_t2_1 - velocidade;
-  y_t2_2 = y_t2_2 - velocidade;
-  y_t2_3 = y_t2_3 - velocidade;
-  y_t3_1 = y_t3_1 - velocidade;
-  y_t3_2 = y_t3_2 - velocidade;
-  y_t3_3 = y_t3_3 - velocidade;
+  //Estrutura metálica do carro
+  strokeWeight (0);
+  fill (255,50,50);
+  rect (posx_c,250,230,70,30,0,30,30);
+  rect (posx_c+80,190,150,60,50,30,0,0);
   
-  fill(100,150);
-  circle (265,y_c1,50);
-  circle (295,y_c2,50);
-  circle (325,y_c3,50);
-  velocidade2 = velocidade2 + aceleracao2;
-  y_c1 = y_c1 + velocidade2;
-  y_c2 = y_c2 + velocidade2;
-  y_c3 = y_c3 + velocidade2;
+  //Rodas cinzas do carro
+  fill(100);
+  circle (posx_c+60,320,60);
+  circle (posx_c+180,320,60);
+  
+  //Farol cinza do carro;
+  ellipse (posx_c+15,285,30,20);
+  
+  //Rodas brancas do carro
+  fill (255);
+  circle (posx_c+60,320,30);
+  circle (posx_c+180,320,30);
+  
+  //Farol branco do carro
+  circle (posx_c+10,285,15);
+  
+  //Janelas e linha da porta do carro
+  strokeWeight (1);
+  fill (200);
+  rect (posx_c+90,200,50,50,50,0,0,0);
+  square (posx_c+170,200,50,0,50,0,0);
+  line (posx_c+155,250,posx_c+155,290);
+  
+  //Obstáculo na rodovia
+  fill (255,200,0);
+  quad (300,290,340,290,360,370,300,370);
+  fill (0);
+  quad (300,320,346,320,350,330,300,330);
+  
+  //Cabeça do Boneco
+  strokeWeight (4);
+  fill (2550);
+  circle (posx_b-15,posy_b,30);
+  
+  //Movimento do carro e do boneco no eixo x
+  posx_b = posx_b - velx_b;
+  if (posx_c>343){
+    posx_c = posx_c - velx_c;
+  }
+  else{
+     //Movimento do boneco no eixo y
+    vely_b = vely_b + gravidade;
+    posy_b = posy_b + vely_b;
+    if (posy_b>330){
+      gravidade = 0;
+      vely_b = 0;
+      velx_b = 0;
+    }
+  }
 }
