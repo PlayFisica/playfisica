f898422fca32746a474bc9f4d47e691d737047ff
GustavoFS <gustavoferreirasantosg@gmail.com>
2025-10-10 14:07:41 -0300

    c23

--- js/scriptMR.js
+++ js/scriptMR.js
@@ -12,3 +12,30 @@ botoes.forEach(btn => {
         document.getElementById(btn.dataset.alvo).classList.add('ativo');
     });
 });
+
+
+
+
+
+
+
+function abrirPagina(endereco) {
+    window.open(endereco, "_blank"); // abre em nova aba
+}
+
+
+
+
+
+function abrirPopup(num) {
+  const popup = document.getElementById("popup" + num);
+  const iframe = popup.querySelector("iframe");
+
+  // Recarrega o iframe sempre que abrir
+  iframe.src = iframe.src;
+  popup.style.display = "flex";
+}
+
+function fecharPopup(num) {
+  document.getElementById("popup" + num).style.display = "none";
+}
